0a2f5be6e7ca2b34f57b8c7264062e3c8627a0d0
Conner <cwestblox@gmail.com>
2017-04-24 17:04:33 -0500

    another fix but to styles this timne

--- game-area.view.component.ts
+++ game-area.view.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-game-area',
-    template: `<div id="area"></div>`,
-    styleUrls: ['./game.component.css']
+    template: `<div id="area"></div>`
+    ]
 })
 export class GameAreaCom implements OnInit {
     constructor() { }
