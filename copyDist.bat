1e62951166580c449af69bfee8ff83d706048351
Conner <cwestblox@gmail.com>
2017-05-01 17:27:20 -0500

    fixed small error change

--- copyDist.bat
+++ copyDist.bat
@@ -5,7 +5,5 @@ MOVE *.js dist\
 MOVE *.map dist\
 XCOPY *.d.ts dist\
 XCOPY *.json dist\*
-echo export * from './2d-gaming.d.ts'; >>dist\index.js
-echo export * from './2d-gaming.d.ts'; >>dist\index.js
 cd dist
 npm publish
